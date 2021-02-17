36c447a3868464d8864a52a8d580c44dba1b1306
TheSorceressofOs <64553635+TheSorceressofOs@users.noreply.github.com>
2021-02-17 22:56:55 +0100

    Add files via upload

---
+++ checksp.sh
@@ -0,0 +1,11 @@
+# script for checking an UNTIS substitution plan, using w3m and various (GNU) utilities (requires fitting plan format)
+# (could be used for e.g. as a cronjob)
+
+# LINKTOPLAN = use the direct link to the plan (ends on .htm)
+# CLASS = your class
+sp=$(w3m LINKTOPLAN | grep -A 1 CLASS | awk '{print $2, $3}' | grep -)&&
+
+#THEDATE = the day of the plan (for e.g. Tomorrow) 
+notify-send -t -1 "Your classes for THEDATE" "$sp"
+
+#end
